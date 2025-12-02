3288dabfe5fe7c95de4f3208e4976a9905f74f3f
evan bergin <berginev@tcd.ie>
2025-12-02 01:17:37 +0000

    Learnt that Data must be inserted into the server and first successful running of app

--- DigEv/app.R
+++ DigEv/app.R
@@ -12,8 +12,9 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(readr)
 
-dig <- read.csv("DIG.csv")
+
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("DIG Trial Shiny Application",
@@ -23,10 +24,10 @@ ui <- navbarPage("DIG Trial Shiny Application",
                h2("About the Dig Trial Introduction"), 
                   p("This Trial Explores the DIG or Digoxin Trial Dataset"),
                     p("Use the tabs above to:"),
-               tags$li("Explore distributions of individual variables"),
-               tags$li("Inversigate the relationships between the variables with interactive points"),
+              tags$ul(tags$li("Explore distributions of individual variables"),
+               tags$li("Investigate the relationships between the variables with interactive points"),
                tags$li("Filter by baseline characteristics")
-               )
+               ))
              ),
     tabPanel("scatterplot",
              fluidPage(h2("AGE vs HR scatterplot"),
@@ -38,6 +39,7 @@ ui <- navbarPage("DIG Trial Shiny Application",
 
 
 server <- function(input,output,session) {output$scatterplot <- renderPlot({
+  dig <- read.csv("DIG.csv")
  ggplot(dig, aes(x = AGE, y = HR)) + 
     geom_point(alpha = 0.5) +
     geom_smooth(method = "loess") +
@@ -46,8 +48,9 @@ server <- function(input,output,session) {output$scatterplot <- renderPlot({
       x = "AGE",
       y = "HR") +
     theme_light()})
-
 }
+
+
 
 # Run the application 
 shinyApp(ui = ui,server = server)
