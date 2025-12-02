b5aea6cf7bfb0d8a82569cc562710b81bc2c1b82
evan bergin <berginev@tcd.ie>
2025-12-02 18:55:17 +0000

    struggling to make the plot work as the app not responding to the DIG.csv data

--- DigEv/app.R
+++ DigEv/app.R
@@ -7,8 +7,6 @@
 #    https://shiny.posit.co/
 #
 
-
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
@@ -18,39 +16,47 @@ library(readr)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("DIG Trial Shiny Application",
+                 
+                 tabPanel("About the Dig Trial",
+                          fluidPage(
+                            h2("About the Dig Trial Introduction"), 
+                            p("This Trial Explores the DIG or Digoxin Trial Dataset"),
+                            p("Use the tabs above to:"),
+                            tags$li("Explore distributions of individual variables"),
+                            tags$li("Investigate the relationships between the variables with interactive points"),
+                            tags$li("Filter by baseline characteristics")
+                          ),
+                          h3("Notes on Data"),
+                          tags$li("Abbreviations and units of measurement for each variable was taken by the DIG code book"),
+                          tags$li("Variables of Significance includes AGE SEX, BMI etc.")
+                          
+                 )
+                 ,
+                 tabPanel("Age vs Treatment",
+                          fluidPage(
+                            h2("AGE vs TRTMT scatterplot"),
+                            plotOutput("Result")
+                          )
+                 ))
 
-    tabPanel("About the Dig Trial",
-             fluidPage(
-               h2("About the Dig Trial Introduction"), 
-                  p("This Trial Explores the DIG or Digoxin Trial Dataset"),
-                    p("Use the tabs above to:"),
-              tags$li("Explore distributions of individual variables"),
-               tags$li("Investigate the relationships between the variables with interactive points"),
-               tags$li("Filter by baseline characteristics")
-               )
-             ),
-    tabPanel("scatterplot",
-             fluidPage(h2("AGE vs HR scatterplot"),
-                       plotOutput("scatterplot")
-                       )
-             )
-    )
-                    
 
-
-server <- function(input,output,session) {output$scatterplot <- renderPlot({
-  dig <- read.csv("DIG.csv")
- ggplot(dig, aes(x = dig$AGE, y = dig$HR)) + 
-    geom_point(alpha = 0.5) +
-    geom_smooth(method = "loess") +
-    labs(
-      title = "AGE vs HR",
-      x = dig$AGE,
-      y = dig$HR) +
-    theme_light()})
-}
-
+    
+   
+    server <- function(input,output,session) {
+      output$Result <- renderPlot({
+        
+        dig <- read.csv("DIG.csv")
+        ggplot(dig.df, aes(x = factor(TRTMT), y = AGE, fill = factor(TRTMT))) +
+          geom_boxplot() +
+          labs(title = "Boxplot of AGE by Treatment",
+               x = "Treatment", y = "Age") +
+          theme_minimal() +
+          scale_fill_manual(values = c("orange2", "pink4"))}
+    
+    )}
+    
 
 
 # Run the application 
 shinyApp(ui = ui,server = server)
+
